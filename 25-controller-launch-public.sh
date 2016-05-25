37d1e48b8c0ec971d3b66661ecf3d7cc631b7056
Van Duc Uy <vducuy@gmail.com>
2016-05-25 16:38:55 +0700

    Add launching script

---
+++ 25-controller-launch-public.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Create the public network
+source admin-openrc.sh
+neutron net-list
+echo -n "Copy public network ID > "
+read PUBLIC_NET_ID
+nova --debug boot public3 --flavor 2 --image Wily --nic net-id=$PUBLIC_NET_ID --key-name new --security-group default
+
+#Create private instance
+#source demo-openrc.sh
+#neutron net-list
+#echo -n "Copy private network ID > "
+#read PRIVATE_NET_ID
+#nova boot private-instance --flavor m1.tiny --image cirros --nic net-id=$PRIVATE_NET_ID --security-group default
+#Check them
+nova list
+
