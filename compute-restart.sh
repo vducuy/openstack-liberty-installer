0fdf5efeea872b0845b87612ace8fd43cf719f25
Van Duc Uy <vducuy@gmail.com>
2016-06-07 16:57:12 +0700

    Add install openvswitch network for compute

--- compute-restart.sh
+++ compute-restart.sh
@@ -1,3 +1,9 @@
-service nova-compute restart
-service neutron-plugin-linuxbridge-agent restart
+service nova-compute stop
+service neutron-plugin-openvswitch-agent stop
+
+rm -f /var/log/nova/*
+rm -f /var/log/neutron/*
+
+service nova-compute start
+service neutron-plugin-openvswitch-agent start
 
